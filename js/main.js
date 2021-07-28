3c364d4c46f2b380c95f5c87499f544149798640
ZLyuka14 <klosiasupatorian@gmail.com>
2021-07-28 11:51:08 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('.quotes__slider').slick({
+		autoplay: true,
+		dots: true,
+		arrows: false
+  });
+
+	$('.header__slider').slick({
+		autoplay: true,
+		dots: true,
+		arrows: true
+  });
+
+});
