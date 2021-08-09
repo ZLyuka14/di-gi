2be2a00280b9312261eb691f4c07b4335a9507b6
ZLyuka14 <klosiasupatorian@gmail.com>
2021-08-09 15:52:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
+	/**
+	 * Slider in "quotes" section
+	 */
 	$('.quotes__slider').slick({
 		autoplay: true,
 		dots: true,
 		arrows: false
-  });
+	});
 
+	/**
+	 * Slider in header
+	 */
 	$('.header__slider').slick({
 		autoplay: true,
 		dots: true,
 		arrows: true
-  });
+	});
 
 });
